b38e5870c47074ba9d7d9061462a46ec2d6a4983
Evbadoloyi Collins Eguasa <collinstb01@gmail.com>
2022-08-29 14:37:31 +0100

    Working on getting Random Number

--- server/contracts/Raffle.sol
+++ server/contracts/Raffle.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Raffle__Enough();
 
@@ -10,9 +11,27 @@ contract Raffle is VRFConsumerBaseV2 {
     uint256 private immutable i_entranceFee;
 
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoorrdinate;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subcriptionid;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private immutable i_numWords;
 
-    constructor(uint256 entranceFee) {
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 numWords,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
+        i_vrfCoorrdinate = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subcriptionid = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        i_numWords = numWords;
     }
 
     event Raffleenter(address indexed player);
@@ -27,9 +46,20 @@ contract Raffle is VRFConsumerBaseV2 {
         emit Raffleenter(msg.sender);
     }
 
-    function requestRandomNumber() external {}
+    function requestRandomNumber() external {
+        i_vrfCoorrdinate.requestRandomWords(
+            i_gasLane,
+            i_subcriptionid,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            i_numWords
+        );
+    }
 
-    function fufillRandomNumber(uint256 requestId, unit256[] memory randomWords) internal virtual {}
+    function fulfillRandomWords(
+        uint256, /* requestId */
+        uint256[] memory randomWords
+    ) internal override {}
 
     function pickRandomWinner() public {}
 
