46d6a0461ca204628dcde6a14147dc9337acd48d
Evbadoloyi Collins Eguasa <collinstb01@gmail.com>
2022-08-29 23:14:32 +0100

    Still On the Raffle Smart Contract

--- server/contracts/Raffle.sol
+++ server/contracts/Raffle.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 
 error Raffle__Enough();
+error Raffle__TransferFailed();
 
-contract Raffle is VRFConsumerBaseV2 {
+contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
     uint256 private immutable i_entranceFee;
 
     address payable[] private s_players;
@@ -16,14 +18,19 @@ contract Raffle is VRFConsumerBaseV2 {
     uint64 private immutable i_subcriptionid;
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private immutable i_callbackGasLimit;
-    uint32 private immutable i_numWords;
+    uint32 private constant numWords = 1;
 
+    // lottery variables
+    // uint256 private immutable i_interval;
+    // uint256 private s_lastTimeStamp;
+    address private s_recentWinner;
+
+    // RaffleState private s_raffleState;
     constructor(
         address vrfCoordinatorV2,
         uint256 entranceFee,
         bytes32 gasLane,
         uint64 subscriptionId,
-        uint32 numWords,
         uint32 callbackGasLimit
     ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
@@ -31,10 +38,11 @@ contract Raffle is VRFConsumerBaseV2 {
         i_gasLane = gasLane;
         i_subcriptionid = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
-        i_numWords = numWords;
     }
 
-    event Raffleenter(address indexed player);
+    event RequestedRaffleWinner(uint256 indexed requestId);
+    event RaffleEnter(address indexed player);
+    event WinnerPicked(address indexed player);
 
     function enterRaffle() public payable {
         // check if the money sent is enough
@@ -43,23 +51,74 @@ contract Raffle is VRFConsumerBaseV2 {
         }
         s_players.push(payable(msg.sender));
         // emit event
-        emit Raffleenter(msg.sender);
+        emit RaffleEnter(msg.sender);
     }
 
+    /**
+     * @dev This is the function that the Chainlink Keeper nodes call
+     * they look for `upkeepNeeded` to return True.
+     * the following should be true for this to return true:
+     * 1. The time interval has passed between raffle runs.
+     * 2. The lottery is open.
+     * 3. The contract has ETH.
+     * 4. Implicity, your subscription is funded with LINK.
+     */
+    function checkUpkeep(
+        bytes memory /* checkData */
+    )
+        public
+        view
+        override
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {}
+
+    /**
+     * @dev Once `checkUpkeep` is returning `true`, this function is called
+     * and it kicks off a Chainlink VRF call to get a random winner.
+     */
+    function performUpkeep(
+        bytes calldata /* performData */
+    ) external override {}
+
+    /**
+     * @dev This is the function that Chainlink VRF node
+     * calls to send the money to the random winner.
+     */
     function requestRandomNumber() external {
-        i_vrfCoorrdinate.requestRandomWords(
+        uint256 requestId = i_vrfCoorrdinate.requestRandomWords(
             i_gasLane,
             i_subcriptionid,
             REQUEST_CONFIRMATIONS,
             i_callbackGasLimit,
-            i_numWords
+            numWords
         );
+        emit RequestedRaffleWinner(requestId);
     }
 
     function fulfillRandomWords(
         uint256, /* requestId */
         uint256[] memory randomWords
-    ) internal override {}
+    ) internal override {
+        // s_players size 10
+        // randomNumber 202
+        // 202 % 10 ? what's doesn't divide evenly into 202?
+        // 20 * 10 = 200
+        // 2
+        // 202 % 10 = 2
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        // s_players = new address payable[](0);
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        // require(success, "Transfer failed");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+        emit WinnerPicked(recentWinner);
+    }
 
     function pickRandomWinner() public {}
 
@@ -69,5 +128,9 @@ contract Raffle is VRFConsumerBaseV2 {
 
     function getPlayer(uint256 index) public view returns (address) {
         return s_players[index];
+    }
+
+    function RecentWinner() public view returns (address) {
+        return s_recentWinner;
     }
 }
